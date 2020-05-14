2674df9f4ac20fd9cc7cdb592532bc8e3e5c1a26
Deepikakolandasamy <deepikakece@gmail.com>
2020-05-14 09:36:00 +0530

    code modified

--- app.py
+++ app.py
@@ -1,48 +1,34 @@
-
+"""
+Clock Exercise for Schlumberger Interview
+"""
 # coding: utf-8
-
-# In[1]:
-
-
-
 
 # In[2]:
 
-
-from flask import Flask, flash, redirect, render_template, request, session
-import random
 import os
+from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='flask_template')
-error=''
-message="e"
-
-
-@app.route('/',methods=['GET'])
+OUTPUT = "e"
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('angle.html',message=message)
-
-@app.route('/user',methods=['POST'])
+    """Go to the HTML template"""
+    return render_template('angle.html', message=OUTPUT)
+@app.route('/user', methods=['POST'])
 def reset():
+    """Calculate the clock function"""
     try:
-        hour=int(request.form['hour'])
-        minute=int(request.form['minute'])
-        if (((hour >= 0) & (hour <= 12)) & ((minute >= 0) & (minute <= 60))):
-            angle=abs((hour * 30 + minute * 0.5)-(minute * 6))
-            message="Angle between the hour and minute: "+str(angle)
-            return render_template('angle.html',message=message)    
+        hour = int(request.form['hour'])
+        minute = int(request.form['minute'])
+        if ((hour >= 0) & (hour <= 12)) & ((minute >= 0) & (minute <= 60)):
+            angle = abs((hour * 30 + minute * 0.5)-(minute * 6))
+            msg = "Angle between the hour and minute: "+str(angle)
         else:
-            message='Enter correct time'
-            return render_template('angle.html',message=message)
-    except:
-        message = "Enter some interger value"
-        return render_template('angle.html',message=message)
-
-
+            msg = "Enter correct interger value"
+        return render_template('angle.html', message=msg)
+    except ValueError:
+        msg = "Enter correct interger value"
+        return render_template('angle.html', message=msg)
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=False,host='0.0.0.0')
-
-
-
-
-
+    app.run(debug=False, host='0.0.0.0')
+ 
